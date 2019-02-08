5cd38b174001653a8113806e7894806ec33d840b
Amanda Hua <amandahua@raspberrypi>
2019-02-08 16:27:51 -0600

    added modification to flash if input is high

--- test.py
+++ test.py
@@ -4,9 +4,11 @@ from time import sleep # Import the sleep function from the time module
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(12, GPIO.IN);
 
 while True:
-	GPIO.output(8, GPIO.HIGH)
-	sleep(1)
-	GPIO.output(8, GPIO.LOW)
-	sleep(1)
+	input = GPIO.input(12)
+	if(input):
+		GPIO.output(8, GPIO.HIGH)
+	else:
+		GPIO.output(8, GPIO.LOW)
